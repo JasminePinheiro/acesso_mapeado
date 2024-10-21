06702d7c1e2c37bf36675d1aa91c00dcbe26f289
Mario Igor <60657808+jumpercs@users.noreply.github.com>
2024-10-21 07:42:48 -0300

    Revert "Feature/jas 17 featregister implementar base de criacao de empresa"

--- lib/pages/onboarding_page.dart
+++ lib/pages/onboarding_page.dart
@@ -1,5 +1,4 @@
 import 'package:acesso_mapeado/pages/sign_in_page.dart';
-import 'package:acesso_mapeado/pages/sign_up_company_page.dart';
 import 'package:acesso_mapeado/pages/sign_up_page.dart';
 import 'package:acesso_mapeado/shared/design_system.dart';
 import 'package:flutter/material.dart';
@@ -112,12 +111,7 @@ class OnboardingPage extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   TextButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const SignUpCompanyPage()));
-                    },
+                    onPressed: () {},
                     child: const Text(
                       'Sou uma empresa',
                       style: TextStyle(
